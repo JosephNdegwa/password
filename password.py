20952f45389cc8bd9c56ccb8ac24c26e5b215146
josephndegwa <kariukindegwa98@gmail.com>
2022-01-24 00:33:39 +0300

    add logic for new user

--- password.py
+++ password.py
@@ -1,3 +1,4 @@
+import email
 from multiprocessing.connection import answer_challenge
 import random
 import pyperclip
@@ -10,4 +11,36 @@ def all():
         print("Please enter your name:")
         username = input()
         print("\n")
-        
+
+        print("Please enter password")
+        password = input()
+        print("\n")
+
+        print("Please enter your email")
+        email = input()
+        print("\n")
+
+
+        handle = open("credentials.txt","a")
+
+        handle.write(username)
+        handle.write(" ")
+        handle.write(password)
+        handle.write(" ")
+        handle.write(email)
+        handle.write("\n")
+
+
+        handle.close()
+        print("Your ccount has been created\n Would you like to explore more?(y/n)")
+        response = input().lower()
+        if response == 'y':
+            generate_account()
+
+        else:
+            print("Get back to us to save a password for you!")
+
+    elif answer == 'y':
+        login()
+
+
