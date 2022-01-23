c1c4525c402d944fe059bed7b453803ce8ec5afb
josephndegwa <kariukindegwa98@gmail.com>
2022-01-24 00:41:54 +0300

    add pass file

--- password.py
+++ password.py
@@ -44,3 +44,21 @@ def all():
         login()
 
 
+
+def generate_account():
+    print("Which account would you like to save a password for?")
+    account_to_be_saved = input()
+    print("Enter 'auto'- for an auto-generated password or 'mine'- to save your own password")
+    choice = input()
+    if choice == 'auto':
+        reversed_account_name = account_to_be_saved[::-1]
+        r = str(random.randint(0,50))
+        password = reversed_account_name + r
+        handle = open("account_and_password.txt","a")
+
+        handle.write(account_to_be_saved)
+        handle.write(" ")
+        handle.write(password)
+        handle.write(" ")
+
+
