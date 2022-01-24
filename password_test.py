0db4a18a3e5c3cd6d0523bae089bd78f260b443c
josephndegwa <kariukindegwa98@gmail.com>
2022-01-24 09:27:23 +0300

    update test

--- password_test.py
+++ password_test.py
@@ -1,2 +1,9 @@
 import unittest
 from password import User
+
+
+def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_User = User("Joseph","Password","	joseph.ndegwa@student.moringaschool.com")
